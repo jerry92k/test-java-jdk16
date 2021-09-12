37353d6efd810b12a60ef386601259c3b3acc9b6
jerry92k <jens92.k@gmail.com>
2021-09-12 21:09:57 +0900

    1. RecordTest 2. DataIoTest 3. ScannerTest

---
+++ src/DataIoTest.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class DataIoTest {
+
+    public static void main(String[] args){
+        DataIoTest dataIoTest = new DataIoTest();
+        String filename="fileoutputtest.txt";
+        dataIoTest.fileOutputStreamTest(filename);
+        dataIoTest.fileWriterTest(filename);
+        dataIoTest.fileInputStreamTest(filename);
+        dataIoTest.fileReadTest(filename);
+    }
+
+    public void fileOutputStreamTest(String filename){
+        String data="this is output Stream test";
+        byte[] bytes = data.getBytes();
+        try (OutputStream out = new FileOutputStream(filename)) {
+            out.write(bytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fileWriterTest(String filename){
+        String data="this is writer test";
+        try(Writer fileWriter=new FileWriter(filename,true)){
+         try(BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)){
+             bufferedWriter.newLine();
+             bufferedWriter.write(data);
+         }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fileInputStreamTest(String filename){
+
+        try(InputStream in = new FileInputStream(filename)) {
+            byte[] bytes = in.readAllBytes(); // 바이너리 데이터를 그대로 읽어서
+            String data = new String(bytes,Charset.defaultCharset()); // 두번째 파라미터로 인코딩 형식을 지정할 수 있음
+            System.out.println("inputstream : "+data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fileReadTest(String filename){
+        try (Reader fileReader=new FileReader(filename)){
+            try(BufferedReader br = new BufferedReader(fileReader)){
+                String line=null;
+                while((line=br.readLine())!=null){
+                    System.out.println("Reader : "+line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ScannerTest.java
@@ -0,0 +1,90 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.MatchResult;
+import java.util.stream.Stream;
+
+public class ScannerTest {
+
+    public static void main(String[] args){
+        ScannerTest scannerTest = new ScannerTest();
+//        scannerTest.scanPathToFileTest();
+//        scannerTest.scanFindAllPatStringTest();
+//        scannerTest.scanFindWitinHorizonTest();
+        scannerTest.scanFindInLine();
+    }
+
+    public void scanPathToFileTest(){
+        Scanner scanner= null;
+        try {
+            scanner = new Scanner(Paths.get("./fileoutputtest.txt"));
+            while(scanner.hasNext()){
+                System.out.println(scanner.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            scanner.close();
+        }
+
+    }
+
+    public void scanFindAllPatStringTest(){
+
+        try(Scanner scanner= new Scanner(Paths.get("./fileoutputtest.txt"))) {
+            // 파일에 없는 패턴을 찾을 경우
+            System.out.println("파일에 없는 단어로 매칭 테스트");
+            Stream<MatchResult> unMatchedResults=scanner.findAll("thos is");
+            System.out.println("matched result size : "+ unMatchedResults.count());
+
+            System.out.println();
+            // 파일에 있는 패턴을 찾을 경우
+            System.out.println("파일에 있는 단어로 매칭 테스트");
+            Stream<MatchResult> matchedResults=scanner.findAll("this is");
+            // 매칭 결과 정보로 MatchResult 객체 2개 생성 확인
+            matchedResults.forEach(result -> System.out.println(result.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void scanFindWitinHorizonTest(){
+        try(Scanner scanner= new Scanner(Paths.get("./fileoutputtest.txt"))) {
+            String result=scanner.findWithinHorizon("this is", 7);
+            System.out.println(result);
+            scanner.next();
+            result=scanner.findWithinHorizon("this is", 7);
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void scanFindInLine(){
+        try(Scanner scanner= new Scanner(Paths.get("./fileoutputtest.txt"))) {
+            String result=null;
+            String result2=null;
+            while(scanner.hasNext()){
+                result=scanner.findInLine("output");
+                System.out.println("find output : " +result);
+                result2=scanner.findInLine("writer");
+                System.out.println("find writer : " +result2);
+                System.out.println();
+                scanner.reset();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
